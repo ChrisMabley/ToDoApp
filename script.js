66df6517f646467401903073a314f7a43f3cf8e8
ChrisMabley <59846867+ChrisMabley@users.noreply.github.com>
2022-11-18 18:02:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -47,10 +47,58 @@ function showList(){
         check.setAttribute("id", a)
         check.setAttribute("onclick","Check(this)");
         close.setAttribute("id", a)
-        close.setAttribute("onclick","delete(this)");
+        close.setAttribute("onclick","close(this)");
         value.innerHTML += " " + list[a].text + " ";
         value.appendChild(check);
         value.appendChild(close);
+        if (list[i].completed){
+            value.style.fontSize = "125%";
+            value.style.textDecoration = "line-through";
+        }
+        else {
+            value.style.color = "black";
+        }
+    }  
+};
+function close(a){
+    event.preventDefault();
+    var xhttpthree = new XMLHttpRequest();
+    if(this.readyState == 4 && this.status == 200){
+        addToDo();
     }
-    
+    else if(this.readyState == 4) {
+        console.log(this.responseText)
+    }
 }
+xhttpthree.open("DELETE", "https://cse204.work/todos" + list[a.id].id, true);
+xhttpthree.setRequestHeader("x-api-key","a86792-0aff99-34671d-e60309-fc5b77");
+xhttpthree.send();
+
+function check(a){
+    event.preventDefault();
+    var xhttpfour = new XMLHttpRequest();
+    xhttpfour.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            var toDos = JSON.parse(this.responseText);
+            addToDo();
+        } 
+        else if (this.readyState == 4){
+            console.log(this.responseText);
+        }
+    }
+    xhttpfour.open("PUT", "https://cse204.work/todos" + list[a.id].id, true);
+    xhttpfour.setRequestHeader("x-api-key","a86792-0aff99-34671d-e60309-fc5b77");
+    xhttpfour.setRequestHeader("Content-type", "application/json");
+
+    var boolean;
+    if(list[a.id].completed){
+        boolean = {completed:false}
+    }
+    else {
+        boolean = {completed: true}
+    }
+    xhttpfour.send(JSON.stringify(boolean));
+};  
+
+const listForm = document.getElementById("addlist");
+listForm.addEventListener("sumbit", add);
